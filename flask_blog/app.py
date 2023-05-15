f4236ac687627c7fbb57c1e9f3663ce34f25e631
Cameron Rebelo <89079757+cameronrebelo@users.noreply.github.com>
2023-05-15 14:30:24 +0200

    Added ability to use /query?query=rice

---
+++ flask_blog/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from urllib.request import urlopen
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/query')
+def get_solr_data():
+    query_param = request.args.get('query')
+    solr_url = 'http://localhost:8983/solr/rice_and_friends/select?q=title%3A'
+    if query_param:
+        solr_url += f'+{query_param}'  # Append the query parameter to the Solr URL
+    solr_url += '*&wt=json'
+    print(solr_url)
+    connection = urlopen(solr_url)
+    response = json.load(connection)
+    num_found = response['response']['numFound']
+    documents = [document.get('title','')
+                 for document in response['response']['docs']]
+    result = {
+        'query' : query_param,
+        'num_found': num_found,
+        'documents': documents
+    }
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run()
