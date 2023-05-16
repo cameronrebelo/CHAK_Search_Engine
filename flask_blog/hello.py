4fe7376a6cd183ff9740f7c68a72fb0d86bfc644
aluciaa <63775053+aluciaa@users.noreply.github.com>
2023-05-16 14:09:48 +0200

    List dispaly

--- flask_blog/hello.py
+++ flask_blog/hello.py
@@ -4,32 +4,36 @@ from urllib.request import urlopen
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         search_input = request.form['search-input']
         if not search_input:
             flash('Title is required!')
-        else:    
+        else:
             return redirect(url_for('index'))
 
     return render_template('solr.html')
+
 
 @app.route('/query')
 def get_solr_data():
     query_param = request.args.get('query')
     solr_url = 'http://localhost:8983/solr/rice_and_friends/select?q=title%3A'
     if query_param:
-        solr_url += f'+{query_param}'  # Append the query parameter to the Solr URL
+        # Append the query parameter to the Solr URL
+        solr_url += f'+{query_param}'
     solr_url += '*&wt=json'
     print(solr_url)
     connection = urlopen(solr_url)
     response = json.load(connection)
     num_found = response['response']['numFound']
-    documents = [document.get('title','')
+    documents = [document.get('title', '')
                  for document in response['response']['docs']]
-    result = {
-        'num_found': num_found,
-        'documents': documents
-    }
-    return jsonify(result)
+
+    return render_template('results.html', documents=documents)
+
+
+if __name__ == '__main__':
+    app.run()
